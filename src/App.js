eecebcbe543e4988df12550d56d76f84b7af3b0c
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-13 23:14:48 +0600

    add navbar drawer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Footer from './Pages/Shared/Footer';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Clean Service</h1>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </Navbar>
+      <Footer></Footer>
     </div>
   );
 }
